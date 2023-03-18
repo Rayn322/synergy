f65c3c010503a765154b89a6740d4cbe783d8f01
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2023-03-17 21:58:02 -0700

    Fix crypto-js imports

--- src/lib/server/encryption.ts
+++ src/lib/server/encryption.ts
@@ -1,10 +1,11 @@
 import { ENCRYPTION_KEY } from '$env/static/private';
-import { AES, enc } from 'crypto-js';
+import utf8 from 'crypto-js/enc-utf8';
+import AES from 'crypto-js/aes';
 
 export function encrypt(text: string) {
 	return AES.encrypt(text, ENCRYPTION_KEY).toString();
 }
 
 export function decrypt(text: string) {
-	return AES.decrypt(text, ENCRYPTION_KEY).toString(enc.Utf8) as string | null;
+	return AES.decrypt(text, ENCRYPTION_KEY).toString(utf8) as string | null;
 }
